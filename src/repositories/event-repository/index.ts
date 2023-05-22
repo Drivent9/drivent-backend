500703f993870fbbcd5bbb87cf51612f11c4833f
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2023-05-22 15:24:04 -0300

    feat: add Redis server to event repository

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,21 @@
-import { prisma } from '@/config';
+import { Event } from '@prisma/client';
+import { prisma, redis } from '@/config';
 
-async function findFirst() {
-  return prisma.event.findFirst();
+const cacheKey = 'event';
+const cacheExpirationInSeconds = 60;
+
+async function findFirst(): Promise<Event> {
+  const cachedEvent = await redis.get(cacheKey);
+
+  if (!cachedEvent) {
+    const event = await prisma.event.findFirst();
+    await redis.set(cacheKey, JSON.stringify(event));
+    await redis.expire(cacheKey, cacheExpirationInSeconds);
+    return event;
+  }
+
+  const event = JSON.parse(cachedEvent);
+  return event;
 }
 
 const eventRepository = {
